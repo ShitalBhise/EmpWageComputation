a4cdc630e78efe9b56363a6457041501e724b330
ShitalBhise <bhiseshital27059@gmail.com>
2022-02-15 12:38:59 +0530

    Added: Part Time Employee and Wage

--- src/com/java/EmpWageCmp.java
+++ src/com/java/EmpWageCmp.java
@@ -3,22 +3,25 @@ package com.java;
 public class EmpWageCmp {
 	public static void main(String[] args)
 {
+		// constants
 		System.out.println("Welcome to Employee Wage Computiong");
 		
-		int IS_FULL_TIME = 1;
+		int IS_FULL_TIME = 2;
+		int IS_PART_TIME = 1;
 		int EMP_RATE_PER_HR = 20;
+		//Variables
 				int empHrs = 0;
 				int empWage = 0;
-				
-		double empcheck = Math.floor(Math.random() * 10)% 2;
+				// Computation
+		double empcheck = Math.floor(Math.random() * 10)% 3;
 		
 		if (empcheck == IS_FULL_TIME)
-			empHrs = 8;
+			empHrs = 16;
 		
 		//	System.out.println("Employee is Present");
-		else
+		else if (empcheck == IS_PART_TIME)
 			//System.out.println("Employee is Absent");
-			empHrs = 0;
+			empHrs = 8;
 		empWage = empHrs * EMP_RATE_PER_HR ;
 		System.out.println("Emp Wage: "  + empWage);
 	
