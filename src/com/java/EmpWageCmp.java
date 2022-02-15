4e22c30e623ab7cb46defa9dd8ff14634112e249
ShitalBhise <bhiseshital27059@gmail.com>
2022-02-15 12:18:02 +0530

    Added: logic for Calculating employee Daily wage

--- src/com/java/EmpWageCmp.java
+++ src/com/java/EmpWageCmp.java
@@ -3,14 +3,24 @@ package com.java;
 public class EmpWageCmp {
 	public static void main(String[] args)
 {
+		System.out.println("Welcome to Employee Wage Computiong");
+		
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HR = 20;
+				int empHrs = 0;
+				int empWage = 0;
+				
 		double empcheck = Math.floor(Math.random() * 10)% 2;
 		
 		if (empcheck == IS_FULL_TIME)
-			System.out.println("Employee is Present");
-		else
-			System.out.println("Employee is Absent");
+			empHrs = 8;
 		
+		//	System.out.println("Employee is Present");
+		else
+			//System.out.println("Employee is Absent");
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HR ;
+		System.out.println("Emp Wage: "  + empWage);
 	
 }
 }
