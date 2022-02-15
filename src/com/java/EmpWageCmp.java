8d15376f8ed23a279159b30e7938e9e88db9021a
ShitalBhise <bhiseshital27059@gmail.com>
2022-02-15 11:44:35 +0530

    Added : Employee present or Absent logics

--- src/com/java/EmpWageCmp.java
+++ src/com/java/EmpWageCmp.java
@@ -1,8 +1,16 @@
 package com.java;
 
 public class EmpWageCmp {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation");
-	}
-
+	public static void main(String[] args)
+{
+		int IS_FULL_TIME = 1;
+		double empcheck = Math.floor(Math.random() * 10)% 2;
+		
+		if (empcheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+		
+	
+}
 }
